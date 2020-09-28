bf6f75533a2310ed33212b41c6c52e791ece163d
1500921255 <1500921255@qq.com>
2020-09-28 23:10:02 +0800

    添加部门信息的组件和导航栏组件

--- oa_system/src/components/left_menu.vue
+++ oa_system/src/components/left_menu.vue
@@ -3,61 +3,58 @@
     <el-col :span="12">
       <el-menu :default-active="this.$router.path" class="el-menu-vertical-demo">
 
-          <router-link to="/index">
-        <el-menu-item index="1">
-          <i class="el-icon-s-home"></i>
+        <router-link to="/index">
+          <el-menu-item index="1">
+            <i class="el-icon-s-home"></i>
             <span slot="title">首页</span>
-        </el-menu-item>
+          </el-menu-item>
         </router-link>
-
 
         <router-link to="/employee_user">
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
+          <el-menu-item index="2">
+            <i class="el-icon-user-solid"></i>
             <span slot="title">人员信息</span>
-        </el-menu-item>
+          </el-menu-item>
         </router-link>
 
-         <router-link to="/user_info">
-        <el-menu-item index="3">
-          <i class="el-icon-document"></i>
-          <span slot="title">个人信息</span>
-        </el-menu-item>
+        <router-link to="/user_info">
+          <el-menu-item index="3">
+            <i class="el-icon-user"></i>
+            <span slot="title">个人信息</span>
+          </el-menu-item>
         </router-link>
-        
+        <router-link to="/department">
+          <el-menu-item index="4">
+            <i class="el-icon-s-custom"></i>
+            <span slot="title">部门信息</span>
+          </el-menu-item>
+        </router-link>
         <router-link to="/calendar">
-        <el-menu-item index="4">
-          <i class="el-icon-date"></i>
-          <span slot="title">日程表</span>
-        </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-date"></i>
+            <span slot="title">日程表</span>
+          </el-menu-item>
         </router-link>
 
         <router-link to="/personage_center">
-        <el-menu-item index="5">
-          <i class="el-icon-setting"></i>
-          <span slot="title">系统监控</span>
-        </el-menu-item>
-        </router-link>
-
-        <router-link to="/sys_log">
-        <el-menu-item index="6">
-          <i class="el-icon-setting"></i>
-          <span slot="title">系统日志</span>
-        </el-menu-item>
+          <el-menu-item index="6">
+            <i class="el-icon-setting"></i>
+            <span slot="title">系统监控</span>
+          </el-menu-item>
         </router-link>
 
         <router-link to="/mailbox">
-        <el-menu-item index="7">
-          <i class="el-icon-message"></i>
-          <span slot="title">站内信箱</span>
-        </el-menu-item>
+          <el-menu-item index="7">
+            <i class="el-icon-message"></i>
+            <span slot="title">站内信箱</span>
+          </el-menu-item>
         </router-link>
 
         <router-link to="/notice">
-        <el-menu-item index="8">
-          <i class="el-icon-message-solid"></i>
-          <span slot="title">通知公告</span>
-        </el-menu-item>
+          <el-menu-item index="8">
+            <i class="el-icon-message-solid"></i>
+            <span slot="title">通知公告</span>
+          </el-menu-item>
         </router-link>
 
       </el-menu>
@@ -68,8 +65,8 @@
   <script>
 export default {
   name: 'leftmenu',
-  method:{
-    native (index){
+  method: {
+    native (index) {
       console.log(index)
     }
   }
@@ -78,7 +75,6 @@ export default {
   </script>
 <style>
 a {
-  text-decoration: none; 
+  text-decoration: none;
 }
-
 </style>
---
+++ oa_system/src/view/department.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-row class="q" style="background: white;height:700px; ">
+    <el-row style="width:96%;height:40px;margin:20px auto;" class="q">
+      <el-col>
+        部门列表
+      </el-col>
+    </el-row>
+    <el-row style="width:94%;height:40px;margin:10px auto;" class="q">
+      <el-col span="18">
+        <el-button type="primary" size="mini" icon="el-icon-edit">新增</el-button>
+      </el-col>
+      <el-col span="6">
+        <el-input v-model="search" size="mini" placeholder="输入关键字搜索" />
+      </el-col>
+    </el-row>
+    <el-table :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      style="width: 94%;margin:0 auto;" class="el-tablelist">
+
+      <el-table-column label="Date" prop="date">
+      </el-table-column>
+      <el-table-column label="Name" prop="name">
+      </el-table-column>
+      <el-table-column align="right">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-row>
+      <el-col style="margin:20px auto;">
+        <el-pagination background layout="prev, pager, next" :total="1000">
+        </el-pagination>
+      </el-col>
+    </el-row>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      search: ''
+    }
+  },
+  methods: {
+    handleEdit (index, row) {
+      console.log(index, row);
+    },
+    handleDelete (index, row) {
+      console.log(index, row);
+    }
+  },
+}
+</script>
+<style>
+.q {
+  font-size: 14px;
+  border-bottom: 1px solid gainsboro;
+  color: rgb(131, 143, 161);
+}
+.el-tablelist {
+  text-align: center;
+}
+.table-title {
+  background: white;
+  border: 1px;
+}
+</style>
